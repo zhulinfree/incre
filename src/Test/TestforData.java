ef8e0be3d91ed118727aa7f9c12103ef586ab3df
zhulinfree <280273861@qq.com>
2018-10-22 16:29:37 +0800

    完整版优化v2

--- src/Test/TestforData.java
+++ src/Test/TestforData.java
@@ -2,6 +2,9 @@ package Test;
 
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import BplusTree.InstanceKey;
 import Data.DataInitial;
 import Data.DataStruct;
@@ -39,10 +42,13 @@ public class TestforData {
 		ArrayList<String> listforKey=new ArrayList<>();
 	
 		listforKey.add("A");
-		
+		listforKey.add("E");
+		listforKey.add("F");
 		InstanceKey key=new InstanceKey(listforKey,objectList.get(tid));
 		
 		int indid=getIndexId(key.getAttrName());
+		System.out.println("index id is "+indid);
+		if(indid==-1) return;
 		
 		System.out.println("pre is");
 		ArrayList<Integer> pre=indexes.getPre(key,indid);
@@ -81,6 +87,11 @@ public class TestforData {
 	}
 	
 	
+	
+	
+	
+	
+	
 	//在ABC上查，有正好的就用正好的，没正好的就用范围大的（AB）,再没有就用ABCD
 	public static int getIndexId(ArrayList<String> todo) {
 		
@@ -98,8 +109,34 @@ public class TestforData {
 		}
 		if(x!=-1) return x;
 		
-		//没有范围大的就用范围小，如ABCD
-		return 0;
+		//没有范围大的就用范围小的，在ABCD上查abc
+		//return 0;
+        System.out.println("\n通过Map.entrySet遍历key和value");  
+        for(Entry<ArrayList<String>, Integer> entry: indexes.indexMap.entrySet())
+        {
+         //System.out.println("Key: "+ entry.getKey()+ " Value: "+entry.getValue());
+        	if(contain(entry.getKey(),todo)) return entry.getValue();
+        }
+		
+        return -1;
+		
+		
+		
 	}
+	
+	//查看l1是否完全包括l2
+	public static boolean contain(ArrayList<String> list1,ArrayList<String> list2) {
+		if(list1.size()==0||list2.size()==0) return false;
+		int count=0;
+		for(String s2:list2) {
+			if(s2.equals(list1.get(count))==false) return false;
+			count++;
+		}
+		
+		return true;
+	}
+	
+	
+	
 		
 }
